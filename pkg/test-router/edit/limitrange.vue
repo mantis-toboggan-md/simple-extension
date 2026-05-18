600d8313bc83fdf1e3dde489b66c66836dec0efc
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2026-05-18 14:06:23 -0700

    initialize a minimal extension to repro useRouter error

---
+++ pkg/test-router/edit/limitrange.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const currentUrl = computed(() => router.currentRoute.value.fullPath);
+</script>
+
+
+<template>
+  <div>{{ currentUrl }}</div>
+</template>
